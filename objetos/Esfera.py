ba38f42e480005e7d604cc1d2891ebc4655d02e3
Paulo Matheus <pmssm@cin.ufpe.br>
2021-06-05 18:01:26 -0300

    feat: estruturas Base/Esfera/plano/reta/seg/tri

---
+++ objetos/Esfera.py
@@ -0,0 +1,4 @@
+class Esfera:
+    def __init__(self, ponto, raio):
+        self.ponto = ponto
+        self.raio = raio
--- objetos/index.py
+++ objetos/index.py
@@ -1,5 +1,12 @@
+# Importando objetos
 from Ponto import Ponto
 from Vetor import Vetor
+from Reta import Reta
+from Segmento import Segmento
+from Triangulo import Triangulo
+from Esfera import Esfera
+from Plano import Plano
+from Base import Base
 
 # istanciar objeto
 ponto = Ponto(1,2,3)
