dc77e90ced016809230a00d9104410ebac671dcc
Jose Guilherme <jgnvs6@gmail.com>
2021-06-06 11:08:34 -0300

    fix: Unresolved import warnings

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
-# Importando objetos
-from Objetos import  Ponto
+from src.index import Ponto
 
 # istanciar objeto
 ponto = Ponto(1,2,3)
 print(ponto.x3)
+
---
+++ src/index.py
@@ -0,0 +1,7 @@
+# Importando objetos
+from src.objetos.Base import  Base
+from src.objetos.Esfera import  Esfera
+from src.objetos.Plano import  Plano
+from src.objetos.Ponto import  Ponto
+from src.objetos.Reta import  Reta
+from src.objetos.Segmento import  Segmento
