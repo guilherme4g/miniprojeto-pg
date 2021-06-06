d59eeb9b3a648a6eb2a45caef4d65f086f673890
Jose Guilherme <jgnvs6@gmail.com>
2021-06-06 14:41:48 -0300

    refactor: rename test file

--- index.py
+++
@@ -1,7 +0,0 @@
-from src.index import Vetor, produtoEscalar
-
-# istanciar objeto
-vetor1 = Vetor(4,2,3)
-vetor2 = "guilherme"
-
-print(produtoEscalar(vetor1, vetor2))
---
+++ testFunctions.py
@@ -0,0 +1,13 @@
+from src.index import Vetor, produtoEscalar
+
+# Tetando função produtoEscalar com vetor invalido
+vetor1 = Vetor(4,2,3)
+vetor2 = Vetor(4,2,-1)
+
+print(produtoEscalar(vetor1, vetor2))
+
+# Tetando função produtoEscalar com vetor invalido
+vetor1 = Vetor(4,2,3)
+vetor2 = "guilherme"
+
+print(produtoEscalar(vetor1, vetor2))
