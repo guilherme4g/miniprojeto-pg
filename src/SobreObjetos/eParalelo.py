34c63bce924de63be95fad905e0791c39934d369
Jose Guilherme <jgnvs6@gmail.com>
2021-06-08 17:32:53 -0300

    feat: eparalelo

---
+++ src/SobreObjetos/eParalelo.py
@@ -0,0 +1,12 @@
+from src.objetos.Vetor import Vetor
+from src.objetos.Reta import Reta
+
+def saoParalelos(vetor, reta):
+    if type(vetor) is not Vetor: raise Exception("argumento vetor não é um vetor valido")
+    if type(reta) is not Reta: raise Exception("argumento reta não é uma reta valido")
+
+    t1 = (vetor.x1 - reta.vetorDiretor.x1) / reta.ponto.x1
+    t2 = (vetor.x2 - reta.vetorDiretor.x2) / reta.ponto.x2
+    t3 = (vetor.x3 - reta.vetorDiretor.x3) / reta.ponto.x3
+
+    return t1 == t2 and t2 == t3
