2cfddb3f28005c25a9cbff028e46013c39960bf0
Jose Guilherme <jgnvs6@gmail.com>
2021-06-09 19:23:53 -0300

    feat: formaCartesiana

---
+++ src/SobreObjetos/formaCartesiana.py
@@ -0,0 +1,9 @@
+from src.objetos.Plano import Plano
+
+def formaCartesiana(plano):
+    if type(plano) is not Plano: raise Exception("argumento reta não é uma reta valido")
+
+    d = (plano.vetorNormal.x1 * (-plano.ponto.x1)) + (plano.vetorNormal.x2 * (-plano.ponto.x2))
+    + (plano.vetorNormal.x3 * (-plano.ponto.x3))
+
+    return [ plano.vetorNormal.x1, plano.vetorNormal.x2, plano.vetorNormal.x3, d]
