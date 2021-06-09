879b6d2e8a76c0b4920739f7986ead91417e4517
Paulo Matheus <pmssm@cin.ufpe.br>
2021-06-09 00:02:13 -0300

    fix: recupera __init__ e corrige projecao e cosseno para usar objetos do tipo Vetor

--- src/FerramentasBasicas/cosseno.py
+++ src/FerramentasBasicas/cosseno.py
@@ -1,18 +1,19 @@
 """função que encontra o angulo entre dois vetores dados"""
 import math
+from src.objetos.Vetor import Vetor
 
 def projecao(v1, v2):
-    quadrado1 = v1[0]**2
-    quadrado2 = v1[1]**2
-    quadrado3 = v1[2]**2
+    quadrado1 = v1.x1**2
+    quadrado2 = v1.x2**2
+    quadrado3 = v1.x3**2
     norma1 = math.sqrt(quadrado1 + quadrado2+ quadrado3)
 
-    quadrado21 = v2[0]**2
-    quadrado22 = v2[1]**2
-    quadrado23 = v2[2]**2
+    quadrado21 = v2.x1**2
+    quadrado22 = v2.x2**2
+    quadrado23 = v2.x3**2
     norma2 = math.sqrt(quadrado21 + quadrado22+ quadrado23)
 
-    produto = v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2]
+    produto = v1.x1 * v2.x1 + v1.x2 * v2.x2 + v1.x3 * v2.x3
 
     print(round(math.degrees(math.acos(produto/(norma1*norma2))), 3))
 
--- src/FerramentasBasicas/projecao.py
+++ src/FerramentasBasicas/projecao.py
@@ -1,14 +1,19 @@
 import math
+from src.objetos.Vetor import Vetor
 
 def projecao(v1, v2):
 
-    produto = v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2]
+    produto = v1.x1 * v2.x2 + v1.x2 * v2.x2 + v1.x3 * v2.x3
 
-    quadrado21 = v2[0]**2
-    quadrado22 = v2[1]**2
-    quadrado23 = v2[2]**2
+    quadrado21 = v2.x1**2
+    quadrado22 = v2.x2**2
+    quadrado23 = v2.x3**2
     norma2 = math.sqrt(quadrado21 + quadrado22 + quadrado23)
 
     const = produto/norma2
-
-    print(list(map(lambda x: round(x * const, 3), v2)))
+    vetor2 = [v2.x1, v2.x2, v2.x3]
+    
+    # v é o vetor projetado em forma de array
+    v = list(map(lambda x: round(x * const, 3), vetor2))
+    vetor = Vetor(v[0], v[1], v[2])
+    return vetor
