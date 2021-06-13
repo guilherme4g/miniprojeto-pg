4a4e97e6fa9dd5bf41b0b0585ea91b3e25c800e3
Paulo Matheus <pmssm@cin.ufpe.br>
2021-06-13 17:30:05 -0300

    fix: corrige projeção

--- src/FerramentasBasicas/projecao.py
+++ src/FerramentasBasicas/projecao.py
@@ -10,9 +10,9 @@ def projecao(v1, v2):
     quadrado23 = v2.x3**2
     norma2 = math.sqrt(quadrado21 + quadrado22 + quadrado23)
 
-    const = produto/norma2
+    const = produto/math.pow(norma2, 2)
     vetor2 = [v2.x1, v2.x2, v2.x3]
-    
+
     # v é o vetor projetado em forma de array
     v = list(map(lambda x: round(x * const, 3), vetor2))
     vetor = Vetor(v[0], v[1], v[2])
