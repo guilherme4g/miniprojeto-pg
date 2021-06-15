dfdab24582a6daceb30892f4ef1e94014e83e278
Jose Guilherme <jgnvs6@gmail.com>
2021-06-14 22:52:08 -0300

    feat: add method multEscalar in Vetor

--- src/Objetos/Vetor.py
+++ src/Objetos/Vetor.py
@@ -6,3 +6,8 @@ class Vetor:
     self.x1 = x1
     self.x2 = x2
     self.x3 = x3
+
+  def multEscalar (self, escalar):
+    self.x1 = self.x1 * escalar
+    self.x2 = self.x2 * escalar
+    self.x3 = self.x3 * escalar
